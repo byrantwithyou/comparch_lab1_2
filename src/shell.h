c66c0a2b97b18fa3639c75ed2ee50157e234c29a
yunxin <martin.yunxin.sun@gmail.com>
2020-10-03 10:28:07 +0200

    rollback shell.c and shell.h

--- src/shell.h
+++ src/shell.h
@@ -19,23 +19,6 @@
 #define FALSE 0
 #define TRUE  1
 
-#define MEM_DATA_START  0x10000000
-#define MEM_DATA_SIZE   0x00100000
-#define MEM_TEXT_START  0x00400000
-#define MEM_TEXT_SIZE   0x00100000
-#define MEM_STACK_START 0x7ff00000
-#define MEM_STACK_SIZE  0x00100000
-#define MEM_KDATA_START 0x90000000
-#define MEM_KDATA_SIZE  0x00100000
-#define MEM_KTEXT_START 0x80000000
-#define MEM_KTEXT_SIZE  0x00100000
-#define MEM_NREGIONS (sizeof(MEM_REGIONS)/sizeof(mem_region_t))
-
-typedef struct {
-    uint32_t start, size;
-    uint8_t *mem;
-} mem_region_t;
-
 extern int RUN_BIT;	/* run bit */
 
 /* only the cache touches these functions */
@@ -44,6 +27,5 @@ void     mem_write_32(uint32_t address, uint32_t value);
 
 /* statistics */
 extern uint32_t stat_cycles, stat_inst_retire, stat_inst_fetch, stat_squash;
-void init_memory();
-void load_program(char *program_filename);
+
 #endif
