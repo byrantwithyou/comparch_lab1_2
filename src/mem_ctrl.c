37ea84e8197063885b3ea22e652f666457cda418
yunxin <martin.yunxin.sun@gmail.com>
2020-10-28 01:44:19 +0100

    working partially

--- src/mem_ctrl.c
+++ src/mem_ctrl.c
@@ -1,29 +1,93 @@
 /*
-* Yunxin Sun 2020 
+* Yunxin Sun 2020
 */
-
-/*
-* Procedure:Mem_Cycle()
-* Purpose: Main Memory Cycle
-*/
-
 #include "cache.h"
 #include "shell.h"
 #include <string.h>
 #include <assert.h>
 
+
+//TODO:serve_request function:set the status to busy, calculate finished_cycle
+//TODO:prune working set status
+//TODO:schedulable function
+//TODO:test
+
+// store which row the 8 bank's row buffer stores, -1 stands for nothing
+int row_buffer[8];
+
+/* ====================Helper Function=============== */
+
+/* 
+* 
+* Procedure: get_row
+* Purpose: get the row id from address
+*
+*/
+int get_row(uint32_t address) {
+    return address >> 16;
+}
+
+/* 
+* 
+* Procedure: get_bank
+* Purpose: get the bank id from address
+*
+*/
+int get_bank(uint32_t address) {
+    return address >> 5 & 0x7;
+}
+
+/* ================================================== */
 /*
 * Procedure: mem_cycle()
 * Purpose: main memory cycle
 */
 void mem_cycle() {
-    l2_cache.mshr.length = 0;
-    for (int i = 0; i < 16; ++i) {
-        l2_cache.mshr.mshr_arr[i].valid = FALSE;
-    }
+    finish_request();
+    MSHR_T *request = find_request_to_serve();
+    // serve request
 }
 
 /* ================= Operation for mshr_queue ================= */
+
+/*
+* Procedure: finish_request()
+* Purpose: find if there're any requests that have been finished and do clean up
+*/
+void finish_request() {
+    for (int i = 0; i < l2_cache.mshr.length; ++i) {
+        if (l2_cache.mshr.mshr_arr[i].finished_cycle == stat_cycles) {
+            out_mshr(l2_cache.mshr.mshr_arr + i);
+        }
+    }
+}
+
+/*
+* Procedure: traverse_msher
+* Purpose: traverse the mshr to find the scheduable and preferred memory request to be served
+*/
+MSHR_T *traverse_mshr(int row_hit) {
+    for (int i = 0; i < l2_cache.mshr.length; ++i) {
+        assert(l2_cache.mshr.mshr_arr[i].valid);
+        MSHR_T *request = l2_cache.mshr.mshr_arr + i;
+        uint32_t address = request->address;
+        int same_row = (get_row(address) == row_buffer[get_bank(address)]);
+        if (schedulable(request) && (!row_hit || same_row)) {
+            return l2_cache.mshr.mshr_arr + i;
+        }
+    }
+    return NULL;
+}
+
+/* 
+* Procedure: find_request_to_serve()
+* Purpose: find the request in the queue to serve
+*/
+MSHR_T *find_request_to_serve() {
+    MSHR_T *request = traverse_mshr(TRUE);
+    if (!request) request = traverse_mshr(FALSE);
+    return request;
+}
 
 /*
 * Procedure: init_mshr()
@@ -33,6 +97,9 @@ void mem_cycle() {
 void init_mshr() {
     l2_cache.mshr.length = 0;
     memset(l2_cache.mshr.mshr_arr, 0, sizeof l2_cache.mshr.mshr_arr);
+    for (int i = 0; i < 8; ++i) {
+        row_buffer[i] = -1;
+    }
 }
 
 /*
@@ -46,7 +113,8 @@ void in_mshr(uint32_t address) {
     l2_cache.mshr.mshr_arr[(*length)++] = (MSHR_T) {
         .valid = TRUE,
         .done = FALSE,
-        .address = address
+        .address = address,
+        .finished_cycle = 0
     };
 }
 
@@ -55,22 +123,17 @@ void in_mshr(uint32_t address) {
 * Purpose: resolve a memory request out of the queue
 *
 */
-void out_mshr(uint32_t address) {
+void out_mshr(MSHR_T *request) {
     int *length = &(l2_cache.mshr.length);
-    assert(((*length) <= 16) && ((*length) > 0));
-    int current_pos = -1;
-    for (int i = 0; i < (*length); ++i) {
-        if ((l2_cache.mshr.mshr_arr[i].valid) && (l2_cache.mshr.mshr_arr[i].address == address)) {
-            current_pos = i;
-            break;
-        }
+    assert(((*length) <= 16) && ((*length) > 0) && request);
+    for (MSHR_T *request_iter = request; request_iter < l2_cache.mshr.mshr_arr + (*length - 1); ++request_iter) {
+        request->address = (request + 1)->address;
+        request->finished_cycle = (request + 1)->finished_cycle;
+        assert(request_iter->valid);
     }
-    assert((current_pos < (*length)) && (current_pos >= 0));
-    for (int i = current_pos; i < (*length) - 1; ++i) {
-        l2_cache.mshr.mshr_arr[i].address = l2_cache.mshr.mshr_arr[i + 1].address;
-        assert(l2_cache.mshr.mshr_arr[i].valid);    
-    }
-    l2_cache.mshr.mshr_arr[--(*length)].valid = FALSE;
+    --(*length);
+    (l2_cache.mshr.mshr_arr + *length) -> valid = FALSE;
+    (l2_cache.mshr.mshr_arr + *length) -> finished_cycle = 0;
 }
 
 /*
@@ -84,4 +147,13 @@ int probe_mshr(uint32_t address) {
         if (l2_cache.mshr.mshr_arr[i].valid && (l2_cache.mshr.mshr_arr[i].address == address)) return TRUE;
     }
     return FALSE;
+}
+
+/* 
+*
+* Procedure: schedulable
+* Purpose: Whether a memory request is schedulable
+*/
+int schedulable(MSHR_T *request) {
+    return TRUE;
 }
--- src/mem_ctrl.h
+++ src/mem_ctrl.h
@@ -5,11 +5,12 @@
 
 #ifndef _MEM_CTRL_H_
 #define _MEM_CTRL_H_
-
-    /* Data Structure for MSHR, this is a queue */
+    
+    /* Data Structure for memory control */
     typedef struct MSHR_T {
         int valid;
         int done;
+        int finished_cycle;
         uint32_t address;
     } MSHR_T;
 
@@ -18,13 +19,34 @@
         MSHR_T mshr_arr[16];
     } MSHR_QUEUE;
 
+    typedef struct RANGE_T {
+        int min_cycle;
+        int max_cycle;
+        struct RANGE_T *next;
+    } RANGE_T;
+
     void mem_cycle();
-    /* Operation for mshr_queue */
+    /* ============Operation for mshr_queue====================== */
     void init_mshr();
     // add an address request into the mshr
     void in_mshr(uint32_t address);
     // when an request is done
-    void out_mshr(uint32_t address);
+    void out_mshr(MSHR_T *request);
     // probe whether an address is in the mshr and is valid
     int probe_mshr(uint32_t address);
+    // whether a request is schedulable
+    int schedulable(MSHR_T *request);
+    // traverse the mshr to find the scheduable and preferred memory request to be served
+    // row_hit: whether to consider row_hit or not
+    MSHR_T *traverse_mshr(int row_hit);
+    // find the request to serve
+    MSHR_T *find_request_to_serve();
+    // find if there're any requests that have been finished and do clean up
+    void finish_request();
+    /* ========================================================= */
+    /* ===============Helper Function=========================== */
+    int get_row(uint32_t address);
+    int get_bank(uint32_t address);
+    /* ========================================================= */
+
 #endif
